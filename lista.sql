369f4d95c803ad94c42401b401fdca4626956062
nicole-santana <nicole.teles.santana@gmail.com>
2023-10-03 18:04:08 -0300

    pt2

--- lista.sql
+++ lista.sql
@@ -11,3 +11,25 @@ select upper(nome) from nomes;
 select length(nome) as tamanho, nome from nomes
 
 
+DELIMITER //
+
+CREATE FUNCTION concatenar(nomes CHAR(20))
+RETURNS CHAR(50)
+BEGIN
+  DECLARE resultado CHAR(50);
+  
+  IF nomes LIKE '%a' THEN
+    SET resultado = CONCAT('Sra. ', nomes);
+  ELSE
+    SET resultado = CONCAT('Sr. ', nomes);
+  END IF;
+
+  RETURN resultado;
+END //
+
+DELIMITER ;
+
+SELECT concatenar('Roberta') AS resultado;
+SELECT concatenar('Roberto') AS resultado;
+SELECT concatenar('Maria Clara') AS resultado;
+SELECT concatenar('João') AS resultado;
